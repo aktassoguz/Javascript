ae9457139c3d3da21b23fa3775627801b8856dbe
aktassoguz <103148203+aktassoguz@users.noreply.github.com>
2022-04-07 00:16:34 +0300

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,12 @@
+console.log('Hello, World!')
+console.log ('Hello','Iam','Oğuz', 2020) // sayıları yazarken tırnak içine almaya gerek yok.
+/* bunu çok satırlı kod 
+yapmak için kullanıyoruz */
+
+
+console.log(2 + 3) // Addition Toplama
+console.log(3 - 2) // Subtraction Çıkarma
+console.log(2 * 3) // Multiplication Çarpma
+console.log(3 / 2) // Division Bölme
+console.log(3 % 2) // Modulus - finding remainder Bölümden kalanı bulma
+console.log(3 ** 2) // Exponentiation 3 ** 2 == 3 * 3  3ün karesi
