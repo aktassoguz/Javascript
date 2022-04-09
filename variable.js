c01aa008b98406c8be03784d52548c374986b1f3
aktassoguz <103148203+aktassoguz@users.noreply.github.com>
2022-04-09 12:46:06 +0300

    Create variable.js

---
+++ variable.js
@@ -0,0 +1,19 @@
+//  Farklı türde verilerin farklı değişkenlerini bildiriyoruz.
+
+let firstName = 'Oguz'    // first name of a person
+let lastName = 'Aktas'      // last name of a person
+let country = 'Turkey'       // country
+let city = 'Istanbul'         // capital city
+let age = 100                 // age in years
+let isMarried = false
+
+// Değişkenleri sayı türüyle belirt
+
+const gravity = 9.81         // earth gravity  in m/s2
+const boilingPoint = 100     // water boiling point, suyun kaynama noktası
+const PI = 3.14              // geometrical constant
+
+// değişkenler ayrıca virgülle ayrılmış satırla belirtilebilir.
+let name = 'Oğuz',     //name of a person
+  job = 'Psychologist',
+  live = 'Turkey'
